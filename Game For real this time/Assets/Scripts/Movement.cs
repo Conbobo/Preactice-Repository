4e1b76ec024bf2006690de8059b3f76663f4cbb1
Conbobo <94762781+Conbobo@users.noreply.github.com>
2022-07-20 14:03:20 -0700

    Made the movement script
    made controls, changed gravity, allowed for the player to move left, right and jump.  Changed some values  like mass, gravity and ground dist.

--- Game For real this time/Assets/Scripts/Movement.cs
+++ Game For real this time/Assets/Scripts/Movement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    public Rigidbody2D player;
+    public float speed;
+    private float playerInput;
+    private bool facingRight = true;
+
+    //Jump section oh noes
+
+    public bool onGround;
+    public Transform feet;
+    public float groundDist;
+    public LayerMask whatIsGround;
+    public float jumpForce;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
+    }
+    void FixedUpdate() {
+        onGround = Physics2D.OverlapCircle(feet.position, groundDist, whatIsGround);
+
+
+        playerInput = Input.GetAxis("Horizontal");
+        player.velocity = new Vector2(speed * playerInput, player.velocity.y);
+        if(playerInput < 0 && facingRight){
+            PlayerFlipper();
+        } else if(playerInput > 0 && !facingRight) {
+            PlayerFlipper();
+        }
+        
+
+    }
+
+    void PlayerFlipper(){
+        facingRight = !facingRight;
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.UpArrow) && onGround) {
+            player.velocity = Vector2.up * jumpForce;
+        }
     }
 }
