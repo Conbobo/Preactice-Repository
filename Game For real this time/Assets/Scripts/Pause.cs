94c26a7f83a9c93cc5ecd8d4ad5f37f7ae89f89c
Conbobo <94762781+Conbobo@users.noreply.github.com>
2022-07-26 13:36:46 -0700

    Camera & Pause
    Fixed wall sticking, camera constraints, pause, backgrounds, area affectors and better platform.  (maybe make speed boost).

---
+++ Game For real this time/Assets/Scripts/Pause.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private bool isPaused = false;
+    public void OnPause(){
+        isPaused = !isPaused;
+        if(isPaused){
+            Time.timeScale = 0f;
+        }else {
+            Time.timeScale = 1f;
+        }
+        
+    }
+}
