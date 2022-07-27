210c56c69e6b7e02319799e7534ab5c44d6655da
Conbobo <94762781+Conbobo@users.noreply.github.com>
2022-07-27 13:38:48 -0700

    Damage and level 4 and lighting
    Added lighting to look nice.  Forgot graphics.  Finished level 4.  Added death.  Added lives.  Added pain.

---
+++ Game For real this time/Assets/Scripts/Death_Behavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Death_Behavior : MonoBehaviour
+{
+    public string level;
+    public Lives lifeManager;
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.CompareTag("Player")){
+            
+            if(lifeManager.loseLife() == 0) {
+                Destroy(other.gameObject);
+                SceneManager.LoadScene(level); 
+            }
+        }
+    }
+}
---
+++ Game For real this time/Assets/Scripts/Lives.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour
+{
+    public GameObject[] lives;
+
+    public int currentLives = 3;
+    
+    public int loseLife(){
+        currentLives--;
+        lives[currentLives].SetActive(false);
+        return currentLives;
+    }
+}
